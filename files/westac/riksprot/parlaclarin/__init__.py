373dd54d16a7452a434e5d79426f12becc00716a
roger-mahler <roger.mahler@hotmail.com>
2022-02-11 11:54:54 +0000

    Deploying to gh-pages from @ roger-mahler/riksprot-jupyterlite@aef8234143771f58be73b1d2bb990cab7a609710 🚀

--- files/westac/riksprot/parlaclarin/__init__.py
+++
@@ -1,9 +0,0 @@
-from .speech_text import SpeechTextRepository
-from .metadata import (
-    MemberNotFoundError,
-    ProtoMetaData,
-    ROLE_TYPE2ID,
-    GENDER2ID,
-    MEMBER_IDNAME2NAME_MAPPING,
-    MEMBER_NAME2IDNAME_MAPPING,
-)
